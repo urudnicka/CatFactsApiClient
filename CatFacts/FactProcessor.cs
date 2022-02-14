adf4d2f6098bd3d17b02e5ff061569afe3f1a78a
Urszula Rudnicka <urudnicka400@gmail.com>
2022-02-14 02:16:15 +0100

    Added FactProcessor class

---
+++ CatFacts/FactProcessor.cs
@@ -0,0 +1,27 @@
+﻿using CatFacts.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CatFacts
+{
+    class FactProcessor
+    {
+        public static async Task<FactModel> LoadFactAsync()
+        {
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync("fact"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    FactModel fact = await response.Content.ReadAsAsync<FactModel>();
+
+                    return fact;
+                }
+                else
+                {
+                    throw new Exception(response.StatusCode + ": " + response.ReasonPhrase); // this or Console.WriteLine
+                }
+            }
+        }
+    }
+}
