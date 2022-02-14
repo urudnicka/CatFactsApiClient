0216be346a4f894e3ffd5c40848a3085a07f1fde
Urszula Rudnicka <urudnicka400@gmail.com>
2022-02-14 03:22:27 +0100

    Initial fact loading

--- CatFacts/Program.cs
+++ CatFacts/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CatFacts.Models;
+using System;
+using System.Threading.Tasks;
 
 namespace CatFacts
 {
@@ -6,9 +8,34 @@ namespace CatFacts
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
+            Console.WriteLine("Do you want to get a cat fact? y/n");
+         
+            if (Console.ReadLine() == "y")
+            {
+                RunAsync().GetAwaiter().GetResult();
+            }
 
         }
+
+        static async Task RunAsync()
+        {
+            ApiHelper.InitialiseClient();
+
+            while (true)
+            {
+                var fact = await FactProcessor.LoadFactAsync();
+
+                Console.WriteLine($"Fact: {fact.Fact}; Length: {fact.Length}");
+
+                Console.WriteLine("Do you want to get another cat fact? y/n");
+
+                if (Console.ReadLine() == "y")
+                    continue;
+                else
+                    break;
+            }
+
+        }
+
     }
 }
